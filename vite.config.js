5fd0c5b0f79ee0c67481f7e18a97f6de9405dcf5
Lordimass <sam@tchrin.net>
2025-04-19 11:11:47 +0100

    Fixed vite-config.js bug
    Edited during switch to TypeScript a couple of commits ago, turned out to not be necessary at all and actually just broke the build process.

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, "src/main.tsx")
-    }
-  }
-})
+export default defineConfig({plugins: [react()]})
