5b586a0c82151edd238f58ccf381392f61dbb196
Lordimass <sam@tchrin.net>
2025-10-08 09:13:55 +0100

    Additional <objectListItem/> dropdown control
    Dropdown toggle can now be controlled externally. The internal toggles can also be hidden independently of this, allowing for more granular control of the component in certain situations.
    
    This will be useful for the "Create Report" button on the reports page.

--- src/components/objectListItem/objectListItem.tsx
+++ src/components/objectListItem/objectListItem.tsx
@@ -4,26 +4,34 @@ import "./objectListItem.css"
 
 /**
  * Renders a list vertically, with content inside specified by a custom component
+ * @param style The style of box to render
+ * @param dropdown Content to include in a drop down menu
+ * @param className Additional class to apply to the item
+ * @param toggleDropdownFunction Ref to set to method to allow the parent component to 
+ * trigger the dropdown externally
+ * @param hideDropdownToggles Whether to hide the "expand"/"collapse" dropdown toggles, 
+ * useful in conjunction with `setTogDropdownFunction`
  */
 export default function ObjectListItem({
-    children, style, dropdown, className
+    children, style, dropdown, className, toggleDropdownFunction,
+    hideDropdownToggles = false
 } : {
     children?: JSX.Element[] | JSX.Element
-    /** The style of box to render */
     style?: "red" | "yellow" | "green"
-    /** Content to include in a drop down menu */
     dropdown?: JSX.Element
-    /** Additional class to apply to the item */
     className?: string
+    toggleDropdownFunction?: React.RefObject<(() => void) | undefined>
+    hideDropdownToggles?: boolean
 }) {
-    function togDropdown() {setDropdownActive(!dropdownActive)}
-
+    function togDropdown() {setDropdownActive(!dropdownActive);}
+    if (toggleDropdownFunction) {toggleDropdownFunction.current = togDropdown;}
+    
     const [dropdownActive, setDropdownActive] = useState(false)
 
     return (<div className={`object-list-item ${className ?? ""}`}>
         <div className={`inner ${style ?? ""}`}>
             {children}
-            {dropdown 
+            {dropdown && !hideDropdownToggles 
                 ? <button className="expand" onClick={togDropdown}>
                     {dropdownActive ? "collapse" : "expand"}
                 </button> 
@@ -32,7 +40,7 @@ export default function ObjectListItem({
         {dropdown && dropdownActive ? 
             <div className="dropdown">
                 {dropdown}
-                <button className="expand" onClick={togDropdown}>collapse</button>
+                {!hideDropdownToggles ? <button className="expand" onClick={togDropdown}>collapse</button> :  null}
             </div> 
         : null}
     </div>)
