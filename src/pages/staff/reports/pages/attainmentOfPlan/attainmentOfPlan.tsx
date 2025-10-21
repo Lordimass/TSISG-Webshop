aeb38e8b87e3ea5497d16dcb6bbb403193bb9894
Lordimass <sam@tchrin.net>
2025-10-21 09:39:26 +0100

    Attainment of Month's Plan Report Page
    Resolves #126

--- src/pages/staff/reports/pages/attainmentOfPlan/attainmentOfPlan.tsx
+++ src/pages/staff/reports/pages/attainmentOfPlan/attainmentOfPlan.tsx
@@ -1,11 +1,30 @@
+import { useContext } from "react"
+import MDXEditorAuth from "../../components/MDXEditorAuth"
 import ReportSubtitle from "../../components/reportSubtitle"
 import "./attainmentOfPlan.css"
+import { ReportContext } from "../../report/lib"
 
 export default function AttainmentOfPlan() {
+    const {report: r, setReportMeta: setR} = useContext(ReportContext)
+    if (!r) return null
     return (<div id="attainment-of-plan-page" className="report-page">
         <ReportSubtitle>
             <h2>Attainment of Month's Plan</h2>
             <p>To what extent did we achieve the goals set out in the previous monthly report?</p>
         </ReportSubtitle>
+         <MDXEditorAuth
+             id="attainment"
+             markdown={r.metadata.attainment ?? ""}
+             background={true}
+             toolbar={true}
+             onChange={(md) => {setR("attainment", md)}}
+         />
+         <MDXEditorAuth
+             id="attainment-box-2"
+             markdown={r.metadata.attainmentBox2 ?? ""}
+             background={true}
+             toolbar={true}
+             onChange={(md) => {setR("attainmentBox2", md)}}
+         />
     </div>)
 }
