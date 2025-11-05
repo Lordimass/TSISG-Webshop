005f6837d6ef42eb930aaa85dc317f58eaf75f45
Lordimass <lordimass@lordimass.net>
2025-11-05 10:38:48 +0000

    Currency Conversion
    Currency conversion and handling using Dinero library and a currency conversion API endpoint.

---
+++ src/components/price/lib.tsx
@@ -0,0 +1,53 @@
+import DineroFactory, {Dinero} from "dinero.js";
+
+type Currency = DineroFactory.Currency
+
+/**
+ * API Endpoint for currency conversion requests.
+ * `{{from}}` will be converted to the currency code to be converted from.
+ * `{{to}}` will be converted to the currency code to be converted to.
+ */
+export const CURRENCY_CONVERSION_ENDPOINT = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{{from}}.min.json"
+
+
+
+type ConversionRates = {
+    /** Date in YYYY-MM-DD format */
+    date: string,
+} & {
+    /**
+     * One {{from}} mapped to many {{to}} currencies
+     * e.g. "gbp": {"1inch": 8.493, "aave": 0.06489, ...}
+     */
+    [key: string]: {
+        [key: string]: number
+    }
+}
+
+async function fetchExchangeRates(from: string): Promise<ConversionRates> {
+    const resp = await fetch(CURRENCY_CONVERSION_ENDPOINT.replace("{{from}}", from.toLowerCase()))
+    if (!resp.ok) {
+        throw new Error("Could not fetch conversion rates")
+    } else {
+        return await resp.json() as ConversionRates
+    }
+}
+
+export async function convertDinero(dinero: Dinero, to: Currency): Promise<Dinero> {
+    let exchangeRates: ConversionRates;
+
+    // Store current exchange rates in `sessionStorage` to save on API calls.
+    let exchangeRatesString = window.sessionStorage.getItem(`exchangeRates${dinero.getCurrency()}`);
+    if (!exchangeRatesString) {
+        exchangeRates = await fetchExchangeRates(dinero.getCurrency());
+        window.sessionStorage.setItem(`exchangeRates${dinero.getCurrency()}`, JSON.stringify(exchangeRates));
+    } else {
+        exchangeRates = JSON.parse(exchangeRatesString);
+    }
+
+    const rate = exchangeRates[dinero.getCurrency().toLowerCase()][to.toLowerCase()]
+    return DineroFactory({
+        amount: Math.round(dinero.getAmount() * rate),
+        currency: to
+    })
+}
